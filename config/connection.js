003615f4d89bea9d8d58683ced27c69098a375ad
jrman523 <jadelramadi@gmail.com>
2019-04-27 14:01:29 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,14 @@ const mysql = require("mysql");
 
 var connection;
 
-if (process.env.ASHSP_URL) {
-  connection = mysql.createConnection(process.env.ASHSP_URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
+    port: 3306,
     password: 'Aa123456',
     database: 'burgers_db'
   });
