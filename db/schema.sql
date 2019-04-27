22259e3e8ed136c9fe70a761e60b3c14dd4cfff1
jrman523 <jadelramadi@gmail.com>
2019-04-27 18:01:34 -0400

    fixed styling

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,10 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-
-CREATE TABLE burgers(
+DROP TABLE IF EXISTS g5uu2rd2dudlezoo.burgers;
+CREATE TABLE g5uu2rd2dudlezoo.burgers(
 id INTEGER AUTO_INCREMENT PRIMARY KEY,
 burger_name VARCHAR(50),
+createdAt TIMESTAMP not null,
 devoured BOOLEAN,
 date TIMESTAMP);
